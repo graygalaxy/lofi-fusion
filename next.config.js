fde4eebaca2082725f41a82be33f436b73bbe20a
Avirup Ghosh <ag.avirup1997@gmail.com>
2022-08-04 10:15:00 +0530

    :package: added svg support

--- next.config.js
+++ next.config.js
@@ -10,6 +10,17 @@ const NextConfig = {
 		reactRemoveProperties: true,
 		removeConsole: !isDev && { exclude: ['error', 'warn'] },
 	},
+	webpack: (config) => {
+		const svgoConfig = { plugins: [{ name: 'removeViewBox', active: false }] }
+		// add support for SVG import
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: { and: [/\.(js|ts|md)x?$/] },
+			use: { loader: '@svgr/webpack', options: { svgoConfig } },
+		})
+
+		return config
+	},
 }
 
 module.exports = NextConfig
