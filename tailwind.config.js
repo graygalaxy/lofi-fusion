d66ce7a3bd3e5a55be9e7b0dcc1f98081f0e4271
Avirup Ghosh <ag.avirup1997@gmail.com>
2022-08-26 21:48:36 +0530

    :wrench: updated tailwind config
    Signed-off-by: Avirup Ghosh <ag.avirup1997@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ['class', '[data-theme="dark"]'],
-	content: ['./**/*.{tsx}'],
+	darkMode: 'class',
+	content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
 	theme: {
 		container: {
 			center: true,
@@ -13,13 +13,8 @@ module.exports = {
 			spacing: {
 				nav: 'var(--nav-height)',
 			},
-			colors: {
-				primary: 'rgba(var(--color-primary), <alpha-value>)',
-				// prettier-ignore
-				gray: {
-					50: '#fafafa', 100: '#f5f5f5', 200: '#e5e5e5', 300: '#d4d4d4', 400: '#a3a3a3',
-					500: '#737373', 600: '#525252', 700: '#404040', 800: '#262626', 900: '#171717'
-				},
+			fontFamily: {
+				roboto: ['Roboto Slab', 'Segoe UI', 'system-ui'],
 			},
 		},
 	},
